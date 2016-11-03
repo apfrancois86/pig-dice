e5ec495c8a4230b0cdf7f63c10e6fed31e42a95f
Alex Francois <apfrancois86@gmail.com>
2016-11-03 11:33:15 -0700

    add ui logic for hold button

--- js/scripts.js
+++ js/scripts.js
@@ -10,7 +10,7 @@ var player1= new Player(0,0,0,true);
 var player2= new Player(0,0,0,false);
 
 Player.prototype.rollDice = function() {
-  this.rollScore = Math.floor(Math.random() * 6);
+  this.rollScore = Math.floor((Math.random() * 6)+1);
 }
 
 Player.prototype.turnScore = function() {
@@ -29,19 +29,42 @@ Player.prototype.playerSwitch = function () {
     this.activePlayer = false;
 }
 
-
+// while (player1.activePlayer === true) {
+//
+// }
 
 //UI logic
 $(function(){
-  (".roll").click(function(){
+  $("#roll").click(function(){
   event.preventDefault();
-
+  $(".playerturn").text(this.Player);
   if (player1.activePlayer === true) {
       player1.rollDice();
       if (player1.rollScore === 1){
         player1.playerSwitch();
-        player2.playerSwitch();
       }
-      player1.roundScoreGenerator(player1.rollScore);
+      player1.turnScore(player1.rollScore);
+      $("#die1").text(player1.rollScore);
+      $("#player1round").text(player1.roundScore);
+
+    } else if (player1.activePlayer === false)  {
+        player2.rollDice();
+          if (player2.rollScore === 1) {
+            player2.playerSwitch();
+            player1.activePlayer = true;
+          }
+        player2.turnScore(player2.rollScore);
+        $("#die1").text(player2.rollScore);
+        $("#player2round").text(player2.roundScore);
+    }
+
+    else {
+     console.log("not working");
+    }
   });
+  $("#hold").click(function(event){
+    event.preventDefault();
+
+  });
+
 });
