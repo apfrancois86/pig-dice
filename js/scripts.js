76c8fb04f44e49a08766827504878b4074b03d39
Alex Francois <apfrancois86@gmail.com>
2016-11-03 09:19:57 -0700

    add total calculator function

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,10 @@
+//business logic
 function Player(totalScore, roundScore, rollScore, activePlayer) {
-  this.totalScore = totalScore; //total amt of points over rounds
-  this.roundScore = roundScore;  //score per turn b4 switching
+  this.totalScore = totalScore; //total amount of points over rounds
+  this.roundScore = roundScore;  //score per turn before switching players
   this.rollScore = rollScore; //current value of die
   this.activePlayer = activePlayer;
+}
 
 var player1= new Player(0,0,0,true);
 var player2= new Player(0,0,0,false);
@@ -11,6 +13,23 @@ Player.prototype.rollDice = function() {
   this.rollScore = Math.floor(Math.random() * 6);
 }
 
-player.prototype.turnScore = function() {
-  
+Player.prototype.turnScore = function() {
+  if (this.rollScore === 1)  {
+    this roundScore === 0;
+  } else {
+    this.roundScore += this.rollScore;
+  }
 }
+
+Player.prototype.totalCalculator = function() {
+  this.totalScore +=this.roundScore;
+}
+
+
+
+
+
+//UI logic
+$(function(){
+
+});
