ec11d9b3134864319ade956c7a149d2bc6b84c0e
Alex Francois <apfrancois86@gmail.com>
2016-11-03 09:01:53 -0700

    add initial html js and readme

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+function Player(totalScore, roundScore, rollScore, activePlayer) {
+  this.totalScore = totalScore; //total amt of points over rounds
+  this.roundScore = roundScore;  //score per turn b4 switching
+  this.rollScore = rollScore; //current value of die
+  this.activePlayer = activePlayer;
+
+var player1= new Player(0,0,0,true);
+var player2= new Player(0,0,0,false);
+
+Player.prototype.rollDice = function() {
+  this.rollScore = Math.floor(Math.random() * 6);
+}
